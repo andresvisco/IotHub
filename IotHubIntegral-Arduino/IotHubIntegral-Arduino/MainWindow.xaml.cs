f180b57679effd3fcf3ed08396d9408235514d11
andresvisco <andres.visco@softtek.com>
2017-07-12 23:09:48 -0300

    Version 2

--- IotHubIntegral-Arduino/IotHubIntegral-Arduino/MainWindow.xaml.cs
+++ IotHubIntegral-Arduino/IotHubIntegral-Arduino/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.IO.Ports;
+using System.Threading;
+using System;
 
 namespace IotHubIntegral_Arduino
 {
@@ -20,9 +10,75 @@ namespace IotHubIntegral_Arduino
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort _sp;
+        Thread _trhead;
+
+        bool flag;
+        string message;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            _sp = new SerialPort();
+
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_sp.IsOpen) {
+                button.Content = "Start";
+                flag = false;
+                _sp.DataReceived -= ReadIn;
+
+
+                _trhead.Join();
+                _sp.Close();
+            }
+            else
+            {
+                button.Content = "stop";
+                _sp.PortName = "COM3";
+                _sp.BaudRate = 9600;
+                
+                _trhead = new Thread(new ThreadStart(Handle));
+                _sp.Open();
+                _sp.DataReceived += new SerialDataReceivedEventHandler(ReadIn);
+                textBox.Text += _sp.ReadLine();
+
+            }
+        }
+
+        private void ReadIn(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!_trhead.IsAlive)
+            {
+                flag = true;
+                _trhead.Start();
+            }
+        }
+
+        private void Handle()
+        {
+            while (flag)
+            {
+                if (_sp.IsOpen)
+                {
+                    try
+                    {
+                        //message = _sp.ReadByte().ToString("X2");
+                        message = _sp.ReadLine().ToString();
+                        Dispatcher.BeginInvoke((Action)(() =>
+                            { textBox.Text += message + " - ";}
+                        ));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
         }
     }
 }
